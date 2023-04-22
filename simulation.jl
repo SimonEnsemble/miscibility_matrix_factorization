4af8ae36892e72cb738eb34f510a197bfb7adc14
Cory Simon <cokes@Corys-MacBook-Pro.local>
2023-04-22 12:09:36 -0700

    progress meter, run checkbox

--- simulation.jl
+++ simulation.jl
@@ -4,12 +4,22 @@
 using Markdown
 using InteractiveUtils
 
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    quote
+        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
+        el
+    end
+end
+
 # ╔═╡ 4305ab70-e080-11ed-1f7c-1b8fb559b6c3
 begin
 	import Pkg; Pkg.activate()
 	push!(LOAD_PATH, "src/")
 	
-	using Revise, PlutoUI, Distributions, DataFrames
+	using Revise, PlutoUI, Distributions, DataFrames, ProgressMeter
 	using MiscibilityMF
 end
 
@@ -44,25 +54,16 @@ viz_miscibility_matrix(raw_data.M_complete, raw_data)
 md"# introduce missing values"
 
 # ╔═╡ 6a1a696c-88e5-46b3-abcc-376ec8099d90
-# ╠═╡ disabled = true
-#=╠═╡
 θ = 0.4 # fraction missing
-  ╠═╡ =#
 
 # ╔═╡ 3c44a682-8161-4b03-aaf0-4d9b813c99cb
-#=╠═╡
 data = sim_data_collection(θ, raw_data, weigh_classes=false)
-  ╠═╡ =#
 
 # ╔═╡ e292d20e-9031-4276-87fb-b59685db2f72
-#=╠═╡
 length(data.ids_obs)
-  ╠═╡ =#
 
 # ╔═╡ 6991d2b0-73df-4b09-aadf-a5877fa5aa5a
-#=╠═╡
 length(data.ids_missing)
-  ╠═╡ =#
 
 # ╔═╡ d2913b8d-ccef-4790-aa69-56106767f592
 md"# dev model
@@ -85,40 +86,28 @@ hyperparams_cv = [(
 			   for _ = 1:5]
 
 # ╔═╡ 4c53ea02-bd91-44a7-9a32-d4759021b7f8
-#=╠═╡
 perf_metrics, opt_hyperparams, fig_losses = do_hyperparam_optimization(data, hyperparams_cv, raw_data, nb_epochs=nb_epochs)
-  ╠═╡ =#
 
 # ╔═╡ 09165856-9117-47e4-8560-fc3f457ad6df
-#=╠═╡
 fig_losses
-  ╠═╡ =#
 
 # ╔═╡ 925791d7-3dee-4d6b-9baa-9ee85afb487c
 md"## train model with opt hyper-params"
 
 # ╔═╡ a3457343-dc4d-4046-83d3-b7bdc20c427c
-#=╠═╡
 model, losses = construct_train_model(opt_hyperparams, data, raw_data, nb_epochs, record_loss=true)
-  ╠═╡ =#
 
 # ╔═╡ a58e7958-458f-4900-8de3-f4eeae945710
-#=╠═╡
 viz_loss(losses)
-  ╠═╡ =#
 
 # ╔═╡ 33e459c3-0d6a-4999-816f-54069bbad86f
-#=╠═╡
 begin
 	set_opt_cutoff!(model, raw_data, data.ids_obs)
 	# model.cutoff = 0.5
 end
-  ╠═╡ =#
 
 # ╔═╡ 143582f4-83dc-4f38-befb-eb0109c37b7f
-#=╠═╡
 viz_latent_space(model, raw_data)
-  ╠═╡ =#
 
 # ╔═╡ 1069ec41-4733-4111-becd-043a104d1c35
 md"
@@ -133,44 +122,41 @@ does not even consider the negative, P(test - | -) = 1 - P(test + | -)
 "
 
 # ╔═╡ cf67313d-8b14-40e1-abfd-ab559450e098
-#=╠═╡
 perf = compute_perf_metrics(model, raw_data, data.ids_missing)
-  ╠═╡ =#
 
 # ╔═╡ 77b9fb17-63e6-4c2c-b0ee-e5919358b4dc
-#=╠═╡
 viz_confusion(perf.cm)
-  ╠═╡ =#
 
 # ╔═╡ 1ce32f38-22e1-43a1-8aa8-49141573208c
 md"## baseline model"
 
 # ╔═╡ ea729838-bdd8-4e17-823d-ac027de562c8
-#=╠═╡
 baseline = test_perf_baseline_model(data, raw_data, set_opt_cutoff=true)
-  ╠═╡ =#
 
 # ╔═╡ f78eccb9-a59f-4d05-a477-bd5ca7ae2e24
-#=╠═╡
 viz_confusion(Float64.(baseline.cm) / 2)
-  ╠═╡ =#
 
 # ╔═╡ 2a85c371-731f-4110-b50a-3d196184f8bb
 md"# multiple runs and sparsities"
+
+# ╔═╡ 05bd9cc6-ce9e-4dec-8c82-d7c62d4d0b6f
+@bind do_multiple_runs CheckBox(default=false)
 
 # ╔═╡ e8221855-745c-4eb1-8320-d785b89c284f
 θs = [0.3, 0.5, 0.7]
 
 # ╔═╡ ea48a8dd-d504-4025-bab4-b2f57e1fd256
-for θ in θs
-	perf_data = run_experiments(θ, raw_data, nruns=10, nb_hyperparams=15)
-
-	p = Gadfly.plot(perf_data, x=:metric, y=:score, color=:model,
-	    Gadfly.Geom.boxplot,
-		Gadfly.Guide.title("θ = $θ"),
-		Gadfly.Coord.cartesian(ymin=0.0, ymax=1.0)
-	)
-	p |> Gadfly.PDF("results_$θ.pdf")
+if do_multiple_runs
+	@showprogress for θ in θs
+		perf_data = run_experiments(θ, raw_data, nruns=10, nb_hyperparams=15)
+	
+		p = Gadfly.plot(perf_data, x=:metric, y=:score, color=:model,
+		    Gadfly.Geom.boxplot,
+			Gadfly.Guide.title("θ = $θ"),
+			Gadfly.Coord.cartesian(ymin=0.0, ymax=1.0)
+		)
+		p |> Gadfly.PDF("results_$θ.pdf")
+	end
 end
 
 # ╔═╡ Cell order:
@@ -206,5 +192,6 @@ end
 # ╠═ea729838-bdd8-4e17-823d-ac027de562c8
 # ╠═f78eccb9-a59f-4d05-a477-bd5ca7ae2e24
 # ╟─2a85c371-731f-4110-b50a-3d196184f8bb
+# ╠═05bd9cc6-ce9e-4dec-8c82-d7c62d4d0b6f
 # ╠═e8221855-745c-4eb1-8320-d785b89c284f
 # ╠═ea48a8dd-d504-4025-bab4-b2f57e1fd256
