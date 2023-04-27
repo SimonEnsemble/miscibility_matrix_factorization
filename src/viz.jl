808550550dd4099f2d29e483bc68667765c7ed8f
Cory Simon <cokes@10-248-165-2.wireless.oregonstate.edu>
2023-04-27 15:41:46 -0700

    tuning plots

--- src/viz.jl
+++ src/viz.jl
@@ -2,30 +2,33 @@ class_to_color = Dict(zip(["Polymer", "Protein", "Surfactant", "Salt"], ColorSch
 class_to_marker = Dict("Polymer"    => :circle,
 					   "Protein"    => :rect,
 					   "Surfactant" => :diamond,
-					   "Salt"       => :hexagon)
+					   "Salt"       => :cross)
+miscibility_colormap = reverse(ColorSchemes.turbid)
 
 function viz_miscibility_matrix(M, raw_data::RawData)
-	hm_colormap = reverse(ColorSchemes.turbid)
-	
-	fig = Figure(resolution=(1200, 1200))
+    big_fontsize = 50
+
+	fig = Figure(resolution=(1400, 1400))
 	ax  = Axis(fig[1, 1], 
 		xlabel="compound", ylabel="compound", 
-		title="miscibility matrix",
 		xgridvisible=false, 
+        xlabelsize=big_fontsize,
+        ylabelsize=big_fontsize,
 		ygridvisible=false, 
 		aspect=DataAspect(),
         xticks=(1:raw_data.n_compounds, raw_data.compounds),
         yticks=(1:raw_data.n_compounds, reverse(raw_data.compounds)),
 		xticklabelrotation=π/2
 	)
-	heatmap!(reverse(M', dims=2), colormap=hm_colormap, aspect=DataAspect())
-	for i = 1:raw_data.n_compounds
+	heatmap!(reverse(M', dims=2), colormap=miscibility_colormap, aspect=DataAspect())
+	for i = 1:raw_data.n_compounds+1
 		hlines!(i - 0.5, color="gray", linewidth=1)
 		vlines!(i - 0.5, color="gray", linewidth=1)
 	end
 	## legend
 	legend_patches = [
-		PolyElement(color=hm_colormap[1], strokecolor="gray"), PolyElement(color=hm_colormap[end], strokecolor="gray")
+		PolyElement(color=miscibility_colormap[1], strokecolor="gray"), 
+        PolyElement(color=miscibility_colormap[end], strokecolor="gray")
 	]
 	legend_labels = ["immiscible", "miscible"]
 	legend_patchsize = (35, 35)
@@ -34,7 +37,7 @@ function viz_miscibility_matrix(M, raw_data::RawData)
 		push!(legend_labels, "missing")
 		legend_patchsize = (35, 35, 35)
 	end
-	Legend(fig[1, 2], legend_patches, legend_labels, patchsize=legend_patchsize)
+	Legend(fig[0, 1], legend_patches, legend_labels, patchsize=legend_patchsize, orientation=:horizontal, labelsize=big_fontsize)
     save("miscibility_matrix.pdf", fig)
 	return fig
 end
@@ -49,7 +52,7 @@ function viz_loss(losses::Vector{Float64})
 	fig
 end
 
-function viz_latent_space(model::MFModel, raw_data::RawData)
+function viz_latent_space(model::MFModel, raw_data::RawData; incl_legend::Bool=true)
 	do_pca = size(model.C)[1] != 2
 
 	if do_pca
@@ -72,16 +75,21 @@ function viz_latent_space(model::MFModel, raw_data::RawData)
 		aspect=DataAspect()
 	)
 
-	vlines!(0.0, color="gray")
-	hlines!(0.0, color="gray")
-	for class in ["Polymer", "Protein", "Surfactant", "Salt"]
+	vlines!(0.0, color="lightgray", linewidth=1, linestyle=:dash)
+	hlines!(0.0, color="lightgray", linewidth=1, linestyle=:dash)
+    sps = []
+	classes = ["Polymer", "Protein", "Surfactant", "Salt"]
+	for class in classes
 		ids = raw_data.classes .== class
-		scatter!(Ĉ[1, ids], Ĉ[2, ids],
-			strokewidth=1, strokecolor="black", marker=class_to_marker[class],
-			color=class_to_color[class], label=class, aspect=DataAspect()
-		)
+		push!(sps,
+              scatter!(Ĉ[1, ids], Ĉ[2, ids],
+                strokewidth=1, strokecolor="black", marker=class_to_marker[class],
+                color=class_to_color[class], label=class, aspect=DataAspect())
+             )
 	end
-	axislegend()
+    if incl_legend
+        Legend(fig[0, 1], sps, classes, orientation=:horizontal)
+    end
     save("latent_space.pdf", fig)
 	return fig
 end
