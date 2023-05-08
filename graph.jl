70ed2fca4259f1219bb14307e0e8c91054e7c475
cory simon <cokes@corys-iMac.local>
2023-05-07 22:13:23 -0700

    get graphmakie to run

--- graph.jl
+++ graph.jl
@@ -34,33 +34,8 @@ function construct_graph(raw_data)
 	return graph
 end
 
-# ╔═╡ 5710f23f-ff98-4ebb-98f4-3c016ff85d3a
+# ╔═╡ 6ca3a2da-9cdb-453f-bb64-6f0941be1175
 graph = construct_graph(raw_data)
-
-# ╔═╡ e927c34e-0dc7-42cf-a97e-d75d775f46a2
-begin
-	function custom_layout(graph)
-		pin = Dict(
-			zip(
-				[findfirst(raw_data.classes .== c) for c in unique(raw_data.classes)],
-				[(5, -5), (-5, 5), (-5, -5), (5, 5)]
-			)
-		)
-		
-		positions = spring(graph, C=7.0, seed=5)#, pin=pin)
-		
-		xs = [pos[1] for pos in positions]
-		ys = [pos[2] for pos in positions]
-		ids_polymer = raw_data.classes .== "Polymer"
-		α = 3.0
-		xs[ids_polymer] *= α
-		ys[ids_polymer] *= α
-		return Point.(zip(xs, ys))
-	end
-end
-
-# ╔═╡ cb6669f6-72cb-4ab5-bd4c-303449f9b8c9
-raw_data.compounds
 
 # ╔═╡ d95c085a-ebf4-4d57-9371-c94219c4c753
 begin
@@ -75,52 +50,21 @@ begin
 	# layout = SFDP(; iterations=200, C=20.0, K=0.5, seed=4, pin=pin)
 	
 	node_color = [MiscibilityMF.class_to_color[class] for class in raw_data.classes]
-	nlabels, nlabels_offset, nlabels_align = nonoverlapping_nlabels(layout)
 	
 	f, ax, p = graphplot(graph, 
 		node_color=node_color, 
 		edge_width=1, 
 		edge_color=("black", 0.5),
 		layout=layout, 
-		nlabels=nlabels, 
-		nlabels_align=nlabels_align,
-		nlabels_offset=nlabels_offset,
+		nlabels=raw_data.compounds, 
+		# nlabels_align=nlabels_align,
+		# nlabels_offset=nlabels_offset,
 		node_attr=(; markersize=20, strokewidth=1)
 	)
-	# hidedecorations!(ax); hidespines!(ax)
+	hidedecorations!(ax); hidespines!(ax)
 	
 	f
 end
-
-# ╔═╡ dcbf750f-aacc-4a10-b096-f154d8e2c55a
-nonoverlapping_nlabels(layout, 0.1)
-
-# ╔═╡ 5e40f523-9b42-450a-8b34-102c6427bacf
-nlabels
-
-# ╔═╡ bf799b73-b3ee-4871-a409-ff23eeae8f51
-rand([3, 4])
-
-# ╔═╡ 6b71b9c0-97d8-4382-acbc-db2c69aa9377
-function draw_graph_class(class::String)
-	graph_c, nodelabel_c = construct_graph(raw_data, class)
-	
-	layout = Spring(;C=10.0, seed=7)
-	# layout = SFDP(; iterations=200, C=0.6, K=1.0, seed=4)
-	f, ax, p = graphplot(graph_c, 
-		node_color=MiscibilityMF.class_to_color[class], 
-		edge_width=1, 
-		edge_color=("black", 0.5),
-		layout=layout, 
-		nlabels=nodelabel_c,
-		node_attr=(; markersize=20, strokewidth=1))
-	# hidedecorations!(ax); hidespines!(ax)
-	
-	f
-end
-
-# ╔═╡ 1fd7b8a0-41a9-4174-bc13-f531e379486e
-graph
 
 # ╔═╡ aa0e3378-633d-494a-a86a-13933984dde6
 function graph_to_dataframe()
@@ -182,9 +126,6 @@ begin
 	end
 end
 
-# ╔═╡ a83a153e-a00b-42fc-8afa-092443deb4ba
-edge_plots
-
 # ╔═╡ b630bb04-511f-4f80-8268-e71bd1c99f6e
 gp = Gadfly.plot(
 	Gadfly.layer(df, x="x", y="y", label="label", color="class",
@@ -202,19 +143,11 @@ Gadfly.draw(Gadfly.SVG("graph.svg", 6Gadfly.inch, 6Gadfly.inch), gp)
 # ╠═13a8cc80-e06d-11ed-3734-dde2e620fda7
 # ╠═f72f5754-e3bb-4ac9-a03b-dbbf38ec196d
 # ╠═f9d05204-d608-4de8-a9f8-d352e18c592d
-# ╠═5710f23f-ff98-4ebb-98f4-3c016ff85d3a
-# ╠═e927c34e-0dc7-42cf-a97e-d75d775f46a2
-# ╠═cb6669f6-72cb-4ab5-bd4c-303449f9b8c9
-# ╠═dcbf750f-aacc-4a10-b096-f154d8e2c55a
+# ╠═6ca3a2da-9cdb-453f-bb64-6f0941be1175
 # ╠═d95c085a-ebf4-4d57-9371-c94219c4c753
-# ╠═5e40f523-9b42-450a-8b34-102c6427bacf
-# ╠═bf799b73-b3ee-4871-a409-ff23eeae8f51
-# ╠═6b71b9c0-97d8-4382-acbc-db2c69aa9377
-# ╠═1fd7b8a0-41a9-4174-bc13-f531e379486e
 # ╠═aa0e3378-633d-494a-a86a-13933984dde6
 # ╠═319ca0bb-040d-46a1-8126-6d87c8b78be0
 # ╠═aa4f2212-15fc-4733-b8f5-84a9ea2b7de9
 # ╠═001f2366-f988-4a0c-a0b4-7692af705c14
-# ╠═a83a153e-a00b-42fc-8afa-092443deb4ba
 # ╠═b630bb04-511f-4f80-8268-e71bd1c99f6e
 # ╠═d6fb82d8-8423-4b59-a2f0-36b389f8ea67
