bb87dcf3d84403ddaaf0dc29ed33272466a8e7da
cory simon <cokes@corys-iMac.local>
2023-05-07 22:10:56 -0700

    gadfly

--- graph.jl
+++ graph.jl
@@ -9,15 +9,12 @@ begin
 	import Pkg; Pkg.activate()
 	push!(LOAD_PATH, "src/")
 
-	using MiscibilityMF, Graphs, GraphMakie, NetworkLayout, CairoMakie, Random, DataFrames
+	using MiscibilityMF, Graphs, GraphMakie, NetworkLayout, CairoMakie, Random, DataFrames, Statistics
 	import Gadfly
 end
 
 # ╔═╡ f72f5754-e3bb-4ac9-a03b-dbbf38ec196d
 raw_data = retreive_raw_data()
-
-# ╔═╡ 7209567b-c832-460d-ac2f-b241b80774c7
-c′=2
 
 # ╔═╡ f9d05204-d608-4de8-a9f8-d352e18c592d
 function construct_graph(raw_data)
@@ -34,33 +31,11 @@ function construct_graph(raw_data)
 		end
 	end
 
-	nodelabel = raw_data.compounds
-	return graph, nodelabel
-end
-
-# ╔═╡ f7c0417a-315b-4921-b307-719d4ba4659d
-count(raw_data.classes .== "Protein")
-
-# ╔═╡ d0bb4e70-ab1b-4cb2-b960-0ef62fe21b6b
-function construct_graph(raw_data, class::String)
-	n_this_class = count(raw_data.classes .== class)
-	
-	# construct the graph
-	graph = SimpleGraph(n_this_class)
-	
-	# loop over pairs of compounds. it's fully connected.
-	for c = 1:n_this_class
-		for c′ = (c+1):n_this_class
-			add_edge!(graph, c, c′)
-		end
-	end
-
-	nodelabel = raw_data.compounds[raw_data.classes .== class]
-	return graph, nodelabel
+	return graph
 end
 
 # ╔═╡ 5710f23f-ff98-4ebb-98f4-3c016ff85d3a
-graph, nodelabel = construct_graph(raw_data)
+graph = construct_graph(raw_data)
 
 # ╔═╡ e927c34e-0dc7-42cf-a97e-d75d775f46a2
 begin
@@ -144,18 +119,43 @@ function draw_graph_class(class::String)
 	f
 end
 
-# ╔═╡ ca4eaf02-461d-40b4-b506-580448883c76
-draw_graph_class("Polymer")
+# ╔═╡ 1fd7b8a0-41a9-4174-bc13-f531e379486e
+graph
 
 # ╔═╡ aa0e3378-633d-494a-a86a-13933984dde6
 function graph_to_dataframe()
-	layout = Spring(;C=10.0, seed=3, pin=pin)
-	return DataFrame(
+	# pin nodes
+	pin = Dict(
+		zip(
+			[findfirst(raw_data.classes .== c) for c in unique(raw_data.classes)],
+			[nothing, (-5, 5), (-5, -5), (5, 5)]
+		)
+	)
+	# compute layout
+	layout = Spring(;C=10.0, seed=3)#, pin=pin)
+	
+	df = DataFrame(
 		x=[pos[1] for pos in layout(graph)],
 		y=[pos[2] for pos in layout(graph)],
 		class=lowercase.(raw_data.classes),
 		label=raw_data.compounds
 	)
+
+	# stretch out polymers
+	polymer_center = mean([
+		filter(row -> row["class"] == "polymer", df)[:, c] 
+		for c in ["x", "y"]]
+	)
+	shift_center = [-20, 15]
+	α = 7.0
+	for i = 1:nrow(df)
+		if df[i, "class"] == "polymer"
+			# sretch
+			df[i, "x"] = shift_center[1] + α * (df[i, "x"] - polymer_center[1])
+			df[i, "y"] = shift_center[2] + α * (df[i, "y"] - polymer_center[2])
+		end
+	end
+	return df
 end
 
 # ╔═╡ 319ca0bb-040d-46a1-8126-6d87c8b78be0
@@ -169,44 +169,39 @@ begin
 	edge_plots = []
 	for class in unique(df[:, "class"])
 		df_c = filter(row -> row["class"] == class, df)
-		xs = []
-		ys = []
-		x_ends = []
-		y_ends = []
-		for i = 1:nrow(df_c)
-			for j = 1:nrow(df_c)
-				push!(xs, df_c[i, "x"])
-				push!(x_ends, df_c[j, "x"])
-				push!(ys, df_c[i, "y"])
-				push!(y_ends, df_c[j, "y"])
-			end
-		end
+		n_c = nrow(df_c)
+		
+		xs =    [df_c[i, "x"] for i = 1:n_c, j = 1:n_c if i < j]
+		ys =    [df_c[i, "y"] for i = 1:n_c, j = 1:n_c if i < j]
+		xends = [df_c[j, "x"] for i = 1:n_c, j = 1:n_c if i < j]
+		yends = [df_c[j, "y"] for i = 1:n_c, j = 1:n_c if i < j]
+		
 		push!(edge_plots, 
-			Gadfly.layer(x=xs, y=ys, xend=x_ends, yend=y_ends, Gadfly.Geom.segment, Gadfly.Theme(default_color=colorant"lightgray"))
+			Gadfly.layer(x=xs, y=ys, xend=xends, yend=yends, Gadfly.Geom.segment, Gadfly.Theme(default_color=colorant"lightgray"))
 		)
 	end
-
 end
 
+# ╔═╡ a83a153e-a00b-42fc-8afa-092443deb4ba
+edge_plots
+
 # ╔═╡ b630bb04-511f-4f80-8268-e71bd1c99f6e
-Gadfly.plot(
+gp = Gadfly.plot(
 	Gadfly.layer(df, x="x", y="y", label="label", color="class",
 		Gadfly.Geom.point, Gadfly.Geom.label),
 	Gadfly.Guide.xlabel(""), Gadfly.Guide.ylabel(""),
 	edge_plots...,
+	Gadfly.Theme(background_color=colorant"white"),
 	Gadfly.Guide.xticks(ticks=nothing), Gadfly.Guide.yticks(ticks=nothing)
 )
 
-# ╔═╡ ec285ff2-ea78-4ed4-a3dd-8e8d384d15f9
-
+# ╔═╡ d6fb82d8-8423-4b59-a2f0-36b389f8ea67
+Gadfly.draw(Gadfly.SVG("graph.svg", 6Gadfly.inch, 6Gadfly.inch), gp)
 
 # ╔═╡ Cell order:
 # ╠═13a8cc80-e06d-11ed-3734-dde2e620fda7
 # ╠═f72f5754-e3bb-4ac9-a03b-dbbf38ec196d
-# ╠═7209567b-c832-460d-ac2f-b241b80774c7
 # ╠═f9d05204-d608-4de8-a9f8-d352e18c592d
-# ╠═f7c0417a-315b-4921-b307-719d4ba4659d
-# ╠═d0bb4e70-ab1b-4cb2-b960-0ef62fe21b6b
 # ╠═5710f23f-ff98-4ebb-98f4-3c016ff85d3a
 # ╠═e927c34e-0dc7-42cf-a97e-d75d775f46a2
 # ╠═cb6669f6-72cb-4ab5-bd4c-303449f9b8c9
@@ -215,10 +210,11 @@ Gadfly.plot(
 # ╠═5e40f523-9b42-450a-8b34-102c6427bacf
 # ╠═bf799b73-b3ee-4871-a409-ff23eeae8f51
 # ╠═6b71b9c0-97d8-4382-acbc-db2c69aa9377
-# ╠═ca4eaf02-461d-40b4-b506-580448883c76
+# ╠═1fd7b8a0-41a9-4174-bc13-f531e379486e
 # ╠═aa0e3378-633d-494a-a86a-13933984dde6
 # ╠═319ca0bb-040d-46a1-8126-6d87c8b78be0
 # ╠═aa4f2212-15fc-4733-b8f5-84a9ea2b7de9
 # ╠═001f2366-f988-4a0c-a0b4-7692af705c14
+# ╠═a83a153e-a00b-42fc-8afa-092443deb4ba
 # ╠═b630bb04-511f-4f80-8268-e71bd1c99f6e
-# ╠═ec285ff2-ea78-4ed4-a3dd-8e8d384d15f9
+# ╠═d6fb82d8-8423-4b59-a2f0-36b389f8ea67
